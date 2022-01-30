2ca041bbb174c3d63e56eff8153f375134d6152c
Martin Bridgman <martin.bridgman@gmail.com>
2022-01-30 05:05:23 +0000

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+const API_KEY = "vxysn23nLJvhpikT4p8MkwwOxXs";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
