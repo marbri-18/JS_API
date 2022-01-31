6b5ac2627460bd70d3d2193720243efc1808b50c
Martin Bridgman <martin.bridgman@gmail.com>
2022-01-31 09:44:20 +0000

    making a post request part 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
+document.getElementById("submit").addEventListener("click", e => postForm(e));
 
 async function getStatus(e) {
     const queryString = `${API_URL}?api_key=${API_KEY}`;
@@ -24,4 +25,16 @@ function displayStatus(data) {
     document.getElementById("resultsModalTitle").innerText = heading;
     document.getElementById("results-content").innerHTML = results;
     resultsModal.show();
+}
+
+async function postForm(e) {
+    const form = new FormData(document.getElementById("checksform"));
+
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+                    "Authorization": API_KEY,
+                    },
+        body: form,
+        })
 }
